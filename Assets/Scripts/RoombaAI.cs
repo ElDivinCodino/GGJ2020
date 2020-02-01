c0eddc97fb302ee76f031619437762dfe6d99c1e
Flu <lucafidao@gmail.com>
2020-02-01 10:59:22 +0100

    Resized assets - Roomba moving

---
+++ Assets/Scripts/RoombaAI.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class RoombaAI : MonoBehaviour
+{
+    //[SerializeField]
+    public Transform destination;
+    public Transform body;
+    public float speedRotation;
+
+    NavMeshAgent navMeshAgent;
+    bool isMoving = false;
+
+    void Start()
+    {
+        navMeshAgent = this.GetComponent<NavMeshAgent>();
+        SetDestination();
+    }
+
+    private void Update()
+    {
+        if (isMoving)
+        {
+            body.Rotate(Vector3.up, speedRotation * Time.deltaTime);
+        }
+    }
+
+    private void SetDestination()
+    {
+        if (destination != null) {
+            Vector3 targetVector = destination.transform.position;
+            navMeshAgent.SetDestination(targetVector);
+            navMeshAgent.enabled = true;
+            isMoving = true;
+        }
+    }
+}
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -7,7 +7,7 @@ public class Spawner : MonoBehaviour
     public GameObject[] cubes;
 
     float posMin, posMax;
-    int toSpawnMin, toSpawnMax, maxElements;
+    int toSpawnMin, toSpawnMax, maxElements, seconds, centralArea;
     //Vector3[] spawnvalues;
 
     List<GameObject> spawned;
@@ -16,35 +16,46 @@ public class Spawner : MonoBehaviour
     void Start()
     {
         spawned = new List<GameObject>();
-        StartCoroutine(DestroyElements());
         posMin = -4.5f;
         posMax = 4.5f;
         toSpawnMin = 5;
         toSpawnMax = 15;
         maxElements = 35;
+        seconds = 1;
+        centralArea = 10;
+
+        StartCoroutine(DestroyElements(seconds));
+        //InitializeEnvironment();
     }
 
-    IEnumerator DestroyElements()
+    //Inizializzazione statica
+    void InitializeEnvironment()
+    {
+        int j = Random.Range(toSpawnMin, toSpawnMax);
+        while (j > 0)
+        {
+            Vector3 position = new Vector3(Mathf.Round(Random.Range(posMin, posMax)), 0, Mathf.Round(Random.Range(posMin, posMax)));
+            while (spawned.Exists(cube => cube.transform.position == position) && ((position.x > -centralArea) && (position.x < centralArea) && (position.y > -centralArea) && (position.y < centralArea)))
+            {
+                position = new Vector3(Mathf.Round(Random.Range(posMin, posMax)), 0, Mathf.Round(Random.Range(posMin, posMax)));
+            }
+            GameObject created = Instantiate(cubes[Random.Range(0, cubes.Length)], position, gameObject.transform.rotation);
+            spawned.Add(created);
+            j--;
+        }
+    }
+
+    //Versione con aggiornamento periodico
+    IEnumerator DestroyElements(int seconds)
     {
         int i = 0;
         while (i < 1000000) {
 
+            InitializeEnvironment();
+
+            yield return new WaitForSeconds(seconds);
+
             int j = Random.Range(toSpawnMin, toSpawnMax);
-            while (j > 0)
-            {
-                Vector3 position = new Vector3(Mathf.Round(Random.Range(posMin, posMax)), 0, Mathf.Round(Random.Range(posMin, posMax)));
-                while (spawned.Exists(cube => cube.transform.position == position ))
-                {
-                    position = new Vector3(Mathf.Round(Random.Range(posMin, posMax)), 0, Mathf.Round(Random.Range(posMin, posMax)));
-                }
-                GameObject created = Instantiate(cubes[Random.Range(0,cubes.Length)], position, gameObject.transform.rotation);
-                spawned.Add(created);
-                j--;
-            }
-
-            yield return new WaitForSeconds(1);
-
-            j = Random.Range(toSpawnMin, toSpawnMax);
             while ((j > 0 || spawned.Count < maxElements) && spawned.Count > 0)
             {
                 GameObject toDestroy = spawned[0];
