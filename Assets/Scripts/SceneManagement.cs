f6a6078857efe022f0b2f3f78c42764fb264c11a
Eldivincodino <francyalz@hotmail.it>
2020-02-01 23:00:50 +0100

    Updated UI

--- Assets/Scripts/SceneManagement.cs
+++ Assets/Scripts/SceneManagement.cs
@@ -6,13 +6,28 @@ using Cinemachine;
 
 public class SceneManagement : MonoBehaviour
 {
+    public float resizeSpeed;
+    public GameObject gameUI;
+
+    bool resize = false;
 
     public void Update()
     {
         if (Input.GetButton("Fire1"))
         {
             GetComponent<Animator>().SetTrigger("Pressed");
-            transform.parent.gameObject.SetActive(false);
+            resize = true;
+            gameUI.SetActive(true);
+        }
+
+        if(resize && transform.parent.localScale.x > 0)
+        {
+            Vector3 scale = transform.parent.localScale - (Vector3.one * Time.deltaTime * resizeSpeed);
+            transform.parent.localScale = scale;
+        }
+        else if (resize)
+        {
+            resize = false;
         }
     }
 }
