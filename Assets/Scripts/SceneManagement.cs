4102b69a18b5a974b47f5f92ac67c9b4abee95f7
Claudio Kerov <ckerov@gmail.com>
2020-02-01 21:56:48 +0100

    Menu transition to gameplay

--- Assets/Scripts/SceneManagement.cs
+++ Assets/Scripts/SceneManagement.cs
@@ -2,11 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using Cinemachine;
 
 public class SceneManagement : MonoBehaviour
 {
-    public void StartGame()
+
+    public void Update()
     {
-        // Fa qualcosa
+        if (Input.GetButton("Fire1"))
+        {
+            GetComponent<Animator>().SetTrigger("Pressed");
+            transform.parent.gameObject.SetActive(false);
+        }
     }
 }
